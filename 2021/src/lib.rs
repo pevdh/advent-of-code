a5547482b3cad093053e7000b3fa0b10c8747c85
Pieter van den Ham <p.e.vandenham@gmail.com>
2021-12-02 14:35:32 +0100

    Improve parser error handling

--- 2021/src/lib.rs
+++ 2021/src/lib.rs
@@ -2,10 +2,12 @@ pub use std::collections::{
     HashMap,
     HashSet
 };
+use nom::error::{convert_error, VerboseError};
+use nom::Finish;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub type ParseResult<'a, S> = nom::IResult<&'a str, S>;
+pub type ParseResult<'a, S> = nom::IResult<&'a str, S, VerboseError<&'a str>>;
 
 pub struct AdventOfCodeSolution<
     I,
@@ -30,11 +32,28 @@ pub fn run<I, P, T1, T2>(solution: AdventOfCodeSolution<I, P, T1, T2>)
 {
     let input_string = read_to_string(solution.day);
 
-    let (_, parsed_test_input) = (solution.parser)(solution.test_input)
-        .expect("Failed to parse test input string");
+    let parsed_test_input = match (solution.parser)(solution.test_input).finish() {
+        Ok((_, parsed_test_input)) => parsed_test_input,
+        Err(e) => {
+            eprintln!(
+                "Errors occurred while parsing test input:\n{}",
+                convert_error(solution.test_input, e)
+            );
+            return;
+        }
+    };
 
-    let (_, parsed_input) =  (solution.parser)(input_string.as_str())
-        .expect("Failed to parse input");
+    let parsed_input = match (solution.parser)(input_string.as_str()).finish() {
+        Ok((_, i)) => i,
+        Err(e) => {
+            eprintln!(
+                "Errors occurred while parsing input from inputs/day_{}.txt:\n{}",
+                solution.day,
+                convert_error(input_string.as_str(), e)
+            );
+            return;
+        }
+    };
 
     let task1_test_output = (solution.task_1)(&parsed_test_input)
         .expect("Error while running task 1 on test input");
