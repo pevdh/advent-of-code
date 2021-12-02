16840ba201ef47905beb45993b111a21db2935ac
Pieter van den Ham <p.e.vandenham@gmail.com>
2021-12-02 06:20:46 +0100

    Finish day 2

--- 2021/src/prelude.rs
+++ 2021/src/prelude.rs
@@ -8,7 +8,7 @@ pub fn read_lines(day: u32)  -> Vec<String>
     let path = format!("input/day_{}.txt", day);
 
     return std::fs::read_to_string(path).unwrap()
-        .split_whitespace()
+        .split("\n")
         .map(|s| s.to_string())
         .collect()
 }
@@ -18,7 +18,7 @@ pub fn read_integers(day: u32)  -> Vec<i64>
     let path = format!("input/day_{}.txt", day);
 
     return std::fs::read_to_string(path).unwrap()
-        .split_whitespace()
+        .split("\n")
         .map(|s| s.parse().unwrap())
         .collect()
 }
