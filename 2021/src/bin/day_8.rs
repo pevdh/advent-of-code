ed1f1455c69dd981033c501160c413abbaa5bc2f
Pieter van den Ham <p.e.vandenham@gmail.com>
2021-12-08 14:04:39 +0100

    Add bruteforce version for fun

--- 2021/src/bin/day_8.rs
+++ 2021/src/bin/day_8.rs
@@ -63,113 +63,167 @@ fn task_1(entries: &[NoteEntry]) -> Result<i32> {
 }
 
 fn task_2(entries: &[NoteEntry]) -> Result<i64> {
-    let mut added_output_values = 0;
+    let mut added_output_values: i64 = 0;
 
     for entry in entries {
-        let signal_patterns = &entry.unique_signal_patterns;
-
-        let mut mapping: HashMap<char, char> = HashMap::new();
-
-        let one = signal_patterns.iter().find(|s| s.len() == DIGITS[1].len())
-            .ok_or(anyhow!("Unable to find one"))?;
-
-        let four = signal_patterns.iter().find(|s| s.len() == DIGITS[4].len())
-            .ok_or(anyhow!("Unable to find four"))?;
-
-        let seven = signal_patterns.iter().find(|s| s.len() == DIGITS[7].len())
-            .ok_or(anyhow!("Unable to find seven"))?;
-
-        let eight = signal_patterns.iter().find(|s| s.len() == DIGITS[8].len())
-            .ok_or(anyhow!("Unable to find eight"))?;
-
-        let a = seven.iter().copied()
-            .find(|&c| !one.iter().any(|&a| a == c))
-            .ok_or(anyhow!("Unable to find \"a\" position"))?;
-
-        mapping.insert(a, 'a');
-
-        let is_six = |s: &[char]| {
-            // Six is the pattern of length 6 that only overlaps one segment with a one
-            s.len() == 6 && overlap(s, one) == 1
-        };
-
-        let six = signal_patterns.iter()
-            .find(|s| is_six(s))
-            .ok_or(anyhow!("Unable to find six"))?;
-
-        // The segment that overlaps in the six and one is f, the segment that does not overlap is c
-        let f = intersection(one, six)
-            .first()
-            .copied()
-            .ok_or(anyhow!("Unable to find f"))?;
-        mapping.insert(f, 'f');
-
-        let c = difference(one, six)
-            .first()
-            .copied()
-            .ok_or(anyhow!("Unable to find c"))?;
-        mapping.insert(c, 'c');
-
-        let is_zero = |s: &[char]| {
-            // Zero is the pattern of length 6 that overlaps with digit 8 for 6 segments
-            // and with digit 4 for 3 segments and it's not the six
-            s.len() == 6 && overlap(eight, s) == 6 && overlap(s, four) == 3 && s != &six[..]
-        };
-
-        let zero = signal_patterns.iter()
-            .find(|s| is_zero(s))
-            .ok_or(anyhow!("Unable to find zero"))?;
-
-        let d = difference(eight, zero)
-            .first()
-            .copied()
-            .ok_or(anyhow!("Unable to find d"))?;
-
-        mapping.insert(d, 'd');
-
-        let is_nine = |s: &[char]| {
-            // Nine is the pattern of length 6 that overlaps with digit 8 for 6 segments
-            // and with digit 4 for 4 segments and it's not the six
-            s.len() == 6 && overlap(eight, s) == 6 && overlap(s, four) == 4 && s != &six[..]
-        };
-
-        let nine = signal_patterns.iter()
-            .find(|s| is_nine(s))
-            .ok_or(anyhow!("Unable to find nine"))?;
-
-        let e = difference(eight, nine)
-            .first()
-            .copied()
-            .ok_or(anyhow!("Unable to find e"))?;
-        mapping.insert(e, 'e');
-
-        // b is the one segment where zero and four overlap, that we haven't mapped yet
-        let b = intersection(zero, four).iter()
-            .find(|c| !mapping.keys().contains(c))
-            .copied()
-            .ok_or(anyhow!("Unable to find b"))?;
-        mapping.insert(b, 'b');
-
-        // The left over segment is g
-        let g = ['a', 'b', 'c', 'd', 'e', 'f', 'g'].iter()
-            .find(|c| !mapping.keys().contains(c))
-            .copied()
-            .ok_or(anyhow!("Unable to find g"))?;
-        mapping.insert(g, 'g');
-
-        for (i, output_signal) in entry.output_value.iter().enumerate() {
-            let mut mapped_output_signal: Vec<char> = output_signal.iter()
-                .map(|c| mapping.get(c).copied().unwrap())
-                .collect();
-            mapped_output_signal.sort_unstable();
-
-            let digit = segments_to_digit(&mapped_output_signal)? as i64;
-
-            added_output_values += digit * 10i64.pow((entry.output_value.len() - i - 1) as u32);
-        }
+        added_output_values += smart_decode(entry)? as i64;
     }
 
     Ok(added_output_values)
+}
+
+#[allow(clippy::many_single_char_names)]
+fn smart_decode(entry: &NoteEntry) -> Result<i32> {
+
+    let signal_patterns = &entry.unique_signal_patterns;
+
+    let mut mapping: HashMap<char, char> = HashMap::new();
+
+    let one = signal_patterns.iter().find(|s| s.len() == DIGITS[1].len())
+        .ok_or(anyhow!("Unable to find one"))?;
+
+    let four = signal_patterns.iter().find(|s| s.len() == DIGITS[4].len())
+        .ok_or(anyhow!("Unable to find four"))?;
+
+    let seven = signal_patterns.iter().find(|s| s.len() == DIGITS[7].len())
+        .ok_or(anyhow!("Unable to find seven"))?;
+
+    let eight = signal_patterns.iter().find(|s| s.len() == DIGITS[8].len())
+        .ok_or(anyhow!("Unable to find eight"))?;
+
+    let a = seven.iter().copied()
+        .find(|&c| !one.iter().any(|&a| a == c))
+        .ok_or(anyhow!("Unable to find \"a\" position"))?;
+
+    mapping.insert(a, 'a');
+
+    let is_six = |s: &[char]| {
+        // Six is the pattern of length 6 that only overlaps one segment with a one
+        s.len() == 6 && overlap(s, one) == 1
+    };
+
+    let six = signal_patterns.iter()
+        .find(|s| is_six(s))
+        .ok_or(anyhow!("Unable to find six"))?;
+
+    // The segment that overlaps in the six and one is f, the segment that does not overlap is c
+    let f = intersection(one, six)
+        .first()
+        .copied()
+        .ok_or(anyhow!("Unable to find f"))?;
+    mapping.insert(f, 'f');
+
+    let c = difference(one, six)
+        .first()
+        .copied()
+        .ok_or(anyhow!("Unable to find c"))?;
+    mapping.insert(c, 'c');
+
+    let is_zero = |s: &[char]| {
+        // Zero is the pattern of length 6 that overlaps with digit 8 for 6 segments
+        // and with digit 4 for 3 segments and it's not the six
+        s.len() == 6 && overlap(eight, s) == 6 && overlap(s, four) == 3 && s != &six[..]
+    };
+
+    let zero = signal_patterns.iter()
+        .find(|s| is_zero(s))
+        .ok_or(anyhow!("Unable to find zero"))?;
+
+    let d = difference(eight, zero)
+        .first()
+        .copied()
+        .ok_or(anyhow!("Unable to find d"))?;
+
+    mapping.insert(d, 'd');
+
+    let is_nine = |s: &[char]| {
+        // Nine is the pattern of length 6 that overlaps with digit 8 for 6 segments
+        // and with digit 4 for 4 segments and it's not the six
+        s.len() == 6 && overlap(eight, s) == 6 && overlap(s, four) == 4 && s != &six[..]
+    };
+
+    let nine = signal_patterns.iter()
+        .find(|s| is_nine(s))
+        .ok_or(anyhow!("Unable to find nine"))?;
+
+    let e = difference(eight, nine)
+        .first()
+        .copied()
+        .ok_or(anyhow!("Unable to find e"))?;
+    mapping.insert(e, 'e');
+
+    // b is the one segment where zero and four overlap, that we haven't mapped yet
+    let b = intersection(zero, four).iter()
+        .find(|c| !mapping.keys().contains(c))
+        .copied()
+        .ok_or(anyhow!("Unable to find b"))?;
+    mapping.insert(b, 'b');
+
+    // The left over segment is g
+    let g = ['a', 'b', 'c', 'd', 'e', 'f', 'g'].iter()
+        .find(|c| !mapping.keys().contains(c))
+        .copied()
+        .ok_or(anyhow!("Unable to find g"))?;
+    mapping.insert(g, 'g');
+
+    let mut value = 0;
+    for (i, output_signal) in entry.output_value.iter().enumerate() {
+        let mut mapped_output_signal: Vec<char> = output_signal.iter()
+            .map(|c| mapping.get(c).copied().unwrap())
+            .collect();
+        mapped_output_signal.sort_unstable();
+
+        let digit = segments_to_digit(&mapped_output_signal)? as i32;
+        value += digit * 10i32.pow((entry.output_value.len() - i - 1) as u32);
+    }
+
+    Ok(value)
+}
+
+#[allow(unused)]
+fn bruteforce_decode(entry: &NoteEntry) -> Result<i32> {
+    let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+    'outer: for perm in chars.iter().permutations(chars.len()) {
+        let mapping: HashMap<char, char> = perm.into_iter()
+            .copied()
+            .zip(chars)
+            .collect();
+
+        for signal in &entry.unique_signal_patterns {
+            let mut mapped_segment: Vec<char> = signal.iter()
+                .map(|c| *mapping.get(c).unwrap())
+                .collect();
+
+            mapped_segment.sort_unstable();
+
+            if segments_to_digit(&mapped_segment).is_err() {
+                continue 'outer;
+            }
+        }
+
+        let mut value = 0;
+        for (i, output_segment) in entry.output_value.iter().enumerate() {
+            let mut mapped_segment: Vec<char> = output_segment.iter()
+                .map(|c| *mapping.get(c).unwrap())
+                .collect();
+
+            mapped_segment.sort_unstable();
+
+            match segments_to_digit(&mapped_segment) {
+                Ok(digit) => {
+                    value += (digit as i32) * 10i32.pow((entry.output_value.len() - i - 1) as u32);
+                }
+                Err(_) => {
+                    continue 'outer;
+                }
+            }
+        }
+
+        return Ok(value);
+    }
+
+    Err(anyhow!("Unable to decode entry"))
 }
 
 fn overlap(a: &[char], b: &[char]) -> usize {
