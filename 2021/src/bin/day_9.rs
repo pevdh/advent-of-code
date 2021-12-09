bb2cfb9a344d03c33ca475957d811b0ec203dfa1
Pieter van den Ham <p.e.vandenham@gmail.com>
2021-12-09 12:59:30 +0100

    Clean up day 9

--- 2021/src/bin/day_9.rs
+++ 2021/src/bin/day_9.rs
@@ -1,6 +1,8 @@
-use ndarray::Array2;
-use aoc2021::*;
 use anyhow::anyhow;
+use aoc2021::*;
+use itertools::Itertools;
+use ndarray::Array2;
+use std::collections::VecDeque;
 
 aoc_main!(
     day: 9,
@@ -17,54 +19,59 @@ aoc_main!(
 );
 
 fn parse(raw_input: &str) -> Result<Array2<u32>> {
-    let cols = raw_input.lines().next().map(|l| l.len())
+    let cols = raw_input
+        .lines()
+        .next()
+        .map(|l| l.len())
         .ok_or(anyhow!("Empty input"))?;
     let rows = raw_input.lines().count();
-
 
     let data: Result<Vec<u32>> = raw_input
         .replace('\n', "")
         .chars()
-        .map(|c| c.to_digit(10).ok_or(anyhow!("Unable to convert char to digit")))
+        .map(|c| {
+            c.to_digit(10)
+                .ok_or(anyhow!("Unable to convert char to digit"))
+        })
         .collect();
 
     Ok(Array2::from_shape_vec((rows, cols), data?)?)
 }
 
-fn task_1(input: &Array2<u32>) -> Result<u32> {
-    let mut result = 0;
-    for low_point in low_points(input) {
-        result += input[low_point] + 1;
-    }
+fn task_1(heightmap: &Array2<u32>) -> Result<u32> {
+    let total_height = low_points(heightmap)
+        .map(|low_point| heightmap[low_point] + 1)
+        .sum();
 
-    Ok(result)
+    Ok(total_height)
 }
 
-fn low_points(input: &Array2<u32>) -> Vec<(usize, usize)> {
-    let mut low_points = vec![];
+fn task_2(heightmap: &Array2<u32>) -> Result<i32> {
+    let total_size = low_points(heightmap)
+        // Find three largest basins and multiply their sizes
+        .map(|low_point| basin_size(low_point, heightmap))
+        .sorted()
+        .rev()
+        .take(3)
+        .fold(1, |sz, prev| prev * sz);
 
-    for i in 0..input.nrows() {
-        for j in 0..input.ncols() {
-            let neighbors = neighbors((i, j), input.nrows(), input.ncols());
-            let height = input[[i, j]];
-
-            let neighbor_min = neighbors.iter()
-                .map(|&(neighbor_row, neighbor_col)| {
-                    input[[neighbor_row, neighbor_col]]
-                })
-                .min()
-                .unwrap();
-
-            if height < neighbor_min {
-                low_points.push((i, j));
-            }
-        }
-    }
-
-    low_points
+    Ok(total_size)
 }
 
-fn neighbors(pos: (usize, usize), nrows: usize, ncols: usize) -> Vec<(usize, usize)> {
+fn low_points(heightmap: &Array2<u32>) -> impl Iterator<Item = (usize, usize)> + '_ {
+    heightmap
+        .indexed_iter()
+        .filter(move |(pos, &height)| {
+            height
+                < neighbors(heightmap, *pos)
+                    .map(|neighbor| heightmap[neighbor])
+                    .min()
+                    .unwrap()
+        })
+        .map(|(pos, _)| pos)
+}
+
+fn neighbors(a: &Array2<u32>, pos: (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
     let row = pos.0 as i32;
     let col = pos.1 as i32;
     let mut v = vec![
@@ -75,43 +82,32 @@ fn neighbors(pos: (usize, usize), nrows: usize, ncols: usize) -> Vec<(usize, usi
     ];
 
     v.retain(|&(neighbor_row, neighbor_col)| {
-        neighbor_row >= 0 && neighbor_col >= 0 && neighbor_row < nrows as i32 && neighbor_col < ncols as i32
+        neighbor_row >= 0
+            && neighbor_col >= 0
+            && neighbor_row < a.nrows() as i32
+            && neighbor_col < a.ncols() as i32
     });
 
-    v.iter().map(|&(a, b)| (a as usize, b as usize)).collect()
+    v.into_iter()
+        .map(|(pos_i, pos_j)| (pos_i as usize, pos_j as usize))
 }
 
-fn task_2(input: &Array2<u32>) -> Result<i32> {
-    let mut basin_sizes = vec![];
+fn basin_size(low_point: (usize, usize), heightmap: &Array2<u32>) -> i32 {
+    let mut to_visit = VecDeque::new();
+    let mut visited = Array2::zeros((heightmap.nrows(), heightmap.ncols()));
 
-    for low_point in low_points(input) {
-        basin_sizes.push(basin_size(low_point, input));
-    }
-
-    basin_sizes.sort_unstable();
-
-    // Find three largest basins and multiply their sizes
-    Ok(basin_sizes.iter().rev()
-        .take(3)
-        .fold(1, |sz, prev| prev * sz))
-}
-
-fn basin_size(low_point: (usize, usize), input: &Array2<u32>) -> i32 {
-    fn basin_size_rec(pos: (usize, usize), input: &Array2<u32>, visited: &mut HashSet<(usize, usize)>) -> i32 {
-        visited.insert(pos.clone());
-
-        let mut neighbor_sizes = vec![];
-        for neighbor in neighbors(pos, input.nrows(), input.ncols()).into_iter()
-            .filter(|&neighbor| input[neighbor] > input[pos] && input[neighbor] < 9) {
-
-            if !visited.contains(&neighbor) {
-                neighbor_sizes.push(basin_size_rec(neighbor, input, visited));
-            }
+    let mut size = 0;
+    to_visit.push_back(low_point);
+    while let Some(current) = to_visit.pop_front() {
+        if visited[current] == 1 {
+            continue;
         }
 
-        return neighbor_sizes.into_iter().sum::<i32>() + 1;
+        visited[current] = 1;
+        size += 1;
+
+        to_visit.extend(neighbors(heightmap, current).filter(|&n| heightmap[n] < 9));
     }
 
-    let mut visited = HashSet::new();
-    return basin_size_rec(low_point, input, &mut visited);
+    size
 }
