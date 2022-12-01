c6bad618ac6d611b52924b8cd0813445c857236a
Pieter van den Ham <p.e.vandenham@gmail.com>
2022-12-01 06:19:29 +0100

    Day 1

--- 2022/src/bin/day_1.rs
+++ 2022/src/bin/day_1.rs
@@ -4,26 +4,60 @@ aoc_main!(
     day: 1,
     test_input:
     r#"
-    test1234
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000
     "#,
     parser: parse,
     task_1: task_1,
-    expected_1: 0,
+    expected_1: 24000,
     task_2: task_2,
-    expected_2: 0,
+    expected_2: 45000,
 );
 
-struct ParsedInput {}
+type ParsedInput = Vec<i32>;
 
 fn parse(raw_input: &str) -> Result<ParsedInput> {
-    println!("\"{}\"", raw_input);
-    todo!();
+    let mut calories = Vec::new();
+
+    let mut total = 0;
+    for line in raw_input.lines() {
+        if line == "" {
+            calories.push(total);
+            total = 0;
+        } else {
+            total += line.parse::<i32>().unwrap();
+        }
+    }
+
+    calories.push(total);
+
+    return Ok(calories);
 }
 
 fn task_1(input: &ParsedInput) -> Result<i32> {
-    todo!();
+    let mut calories = input.clone();
+    calories.sort();
+    calories.reverse();
+
+    return Ok(calories[0]);
 }
 
 fn task_2(input: &ParsedInput) -> Result<i32> {
-    todo!();
+    let mut calories = input.clone();
+    calories.sort();
+    calories.reverse();
+
+    return Ok(calories[0..3].iter().sum());
 }
