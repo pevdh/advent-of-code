43dd193aeed492b21ecfb80ce8e4cd3be37316ec
Pieter van den Ham <p.e.vandenham@gmail.com>
2024-11-29 23:42:55 +0000

    AoC2024: Preparing to save Christmas once again

---
+++ 2024/src/bin/day_1.rs
@@ -0,0 +1,19 @@
+use aoc2024::*;
+
+aoc_main!(
+    day: 1,
+    test_input: r#""#,
+    test_input_2: r#""#,
+    task_1: task_1,
+    expected_1: 142,
+    task_2: task_2,
+    expected_2: 281,
+);
+
+fn task_1(input: &str) -> Result<i64> {
+    Err(eyre!("Not implemented"))
+}
+
+fn task_2(input: &str) -> Result<i64> {
+    Err(eyre!("Not implemented"))
+}
